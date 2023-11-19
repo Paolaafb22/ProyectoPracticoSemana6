fda17cb3a0019a7441834c6ffeb66e89bbc16bd2
Paola_Forero <p.forerob@uniandes.edu.co>
2023-11-19 06:41:17 -0500

    versión 2

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Prueba practica semana 6!');
+      expect(appController.getHello()).toBe('Prueba practica Diseño y construcción de APIs semana 6!');
     });
   });
 });
